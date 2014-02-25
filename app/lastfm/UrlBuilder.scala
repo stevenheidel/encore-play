00bb5910b295a2b54f08d067aa09f42fee6f14b4
Steven Heidel <steven@livingskyweb.ca>
2014-02-24 20:29:10 -0600

    Convert to scala-uri library

--- app/lastfm/UrlBuilder.scala
+++ app/lastfm/UrlBuilder.scala
@@ -2,11 +2,24 @@ package lastfm
 
 import play.api.Play
 
+import com.netaporter.uri.dsl._
+
 object UrlBuilder {
+
   val apiKey = Play.current.configuration.getString("lastfm.key").get
-  val baseUrl = "http://ws.audioscrobbler.com/2.0/"
+  val apiVersion = "2.0"
+  val apiFormat = "json"
+
+  val baseUrl = "http://ws.audioscrobbler.com" / apiVersion
+  val baseParams = ("format" -> apiFormat) :: ("api_key" -> apiKey) :: Nil
 
   def event_getInfo(eventId: Long): String = {
-    baseUrl + "?method=event.getInfo&event=" + eventId.toString + "&api_key=" + apiKey + "&format=json"
+    constructUrl("event.getInfo", ("event" -> eventId) :: Nil)
   }
+
+  private def constructUrl(method: String, params: List[Tuple2[String, Any]]) = {
+    val allParams = ("method" -> method) :: params ::: baseParams
+    baseUrl.addParams(allParams).toString
+  }
+
 }
