c1b1e8fa5cd8ced08f9252fd2e9d31642b3da056
Steven Heidel <steven@livingskyweb.ca>
2014-02-24 23:25:27 -0600

    Cache event responses with Reactive Mongo

--- app/controllers/Events.scala
+++ app/controllers/Events.scala
@@ -3,17 +3,46 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.api.libs.ws._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
 import scala.concurrent.Future
+
+// Reactive Mongo imports
+import reactivemongo.api._
+
+// Reactive Mongo plugin, including the JSON-specialized collection
+import play.modules.reactivemongo.MongoController
+import play.modules.reactivemongo.json.collection.JSONCollection
 
 import lastfm.UrlBuilder
 
-object Events extends Controller {
+object Events extends Controller with MongoController {
 
-  implicit val context = scala.concurrent.ExecutionContext.Implicits.global
+  def collection: JSONCollection = db.collection[JSONCollection]("responses")
 
-  def get(id: Long) = Action.async {
-    WS.url(UrlBuilder.event_getInfo(id)).get().map { response =>
-      Ok(response.json)
+  def get(id: Long) = {
+    // Prepare the response from Lastfm
+    val response: Future[Response] = WS.url(UrlBuilder.event_getInfo(id)).get()
+
+    // Check to see if the response is in the database
+    val dbCursor: Cursor[JsObject] = collection.find(Json.obj("event.id" -> id.toString)).cursor[JsObject]
+    val dbList: Future[List[JsObject]] = dbCursor.collect[List]()
+
+    Action.async {
+      dbList.flatMap { list =>
+        if (list.isEmpty) {
+          response.map { result =>
+            collection.insert(result.json).map(lastError =>
+              Ok("Mongo LastError: %s".format(lastError)))
+
+            Ok(result.json)
+          }
+        }
+        else {
+          Future(Ok(list.head))
+        }
+      }
     }
   }
 
