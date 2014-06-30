31196f0a8805005ab5436257254565e698dbb189
Steven Heidel <steven@livingskyweb.ca>
2014-06-30 17:35:36 -0600

    Add logger for redirects

--- app/controllers/RedirectController.scala
+++ app/controllers/RedirectController.scala
@@ -1,13 +1,16 @@
 package controllers
 
+import play.api._
 import play.api.mvc._
 
 object RedirectController extends Controller {
 
   def redirect(path: String) = Action { implicit request =>
     val queryString: String = if (request.rawQueryString.nonEmpty) "?" + request.rawQueryString else ""
+    val url: String = "http://on.encore.fm/api/v1/" + path + queryString
 
-    TemporaryRedirect("http://on.encore.fm/api/v1/" + path + queryString )
+    Logger.info("Redirected to: " + url)
+    TemporaryRedirect(url)
   }
 
 }
