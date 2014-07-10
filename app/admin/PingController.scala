4b70db86486b4a6c27ae90ff22683f97e6e79f88
Steven Heidel <steven@livingskyweb.ca>
2014-07-10 00:08:31 -0600

    Have ping controller test database connection too

--- app/admin/PingController.scala
+++ app/admin/PingController.scala
@@ -2,7 +2,19 @@ package admin
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json._
+import scala.concurrent.Future
+import reactivemongo.api._
+import play.modules.reactivemongo.MongoController
+import play.modules.reactivemongo.json.collection.JSONCollection
 
-object PingController extends Controller {
-  def ping = Action(Ok("pong"))
+object PingController extends Controller with MongoController {
+  def collection: JSONCollection = db.collection[JSONCollection]("ping")
+
+  def ping = Action.async {
+    collection.find(Json.obj("ping" -> "ping")).one[JsValue].map { p =>
+      Ok(p.get)
+    }
+  }
 }
