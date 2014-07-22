ae12d58f95d51d746b2b7d408622cd7f94faa079
Steven Heidel <steven@livingskyweb.ca>
2014-07-22 01:37:07 -0600

    Make JSON errors

--- app/Global.scala
+++ app/Global.scala
@@ -2,19 +2,23 @@ import play.api._
 import play.api.mvc._
 import play.api.mvc.Results._
 import scala.concurrent.Future
+import play.api.libs.json._
 
 object Global extends GlobalSettings {
 
   override def onError(request: RequestHeader, ex: Throwable) = {
-    Future.successful(InternalServerError("Internal Server Error: " + ex))
+    val msg: String = "Internal Server Error: " + ex
+    Future.successful(InternalServerError(Json.obj("error" -> msg)))
   }
 
   override def onHandlerNotFound(request: RequestHeader) = {
-    Future.successful(NotFound("Not Found: " + request.path))
+    val msg: String = "Not Found: " + request.path
+    Future.successful(NotFound(Json.obj("error" -> msg)))
   }
 
   override def onBadRequest(request: RequestHeader, error: String) = {
-    Future.successful(BadRequest("Bad Request: " + error))
+    val msg: String = "Bad Request: " + error
+    Future.successful(BadRequest(Json.obj("error" -> msg)))
   }
 
 }
