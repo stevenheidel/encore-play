428456a8d561011d423a4757ddd48082e2d319d1
Steven Heidel <steven@livingskyweb.ca>
2014-07-23 18:50:36 -0600

    Use the database to store the currently populating events, as opposed to a mutable set. This allows scaling to multiple dynos that don't support Akka remoting

--- app/populator/actors/PopulatorActor.scala
+++ app/populator/actors/PopulatorActor.scala
@@ -10,17 +10,22 @@ import play.api.Play.current
 import populator.algorithms._
 import scala.concurrent.Future
 import scala.util.{Success, Failure}
+import play.modules.reactivemongo.ReactiveMongoPlugin
+import play.modules.reactivemongo.json.collection.JSONCollection
+import reactivemongo.api._
+import scala.concurrent.Future
+import play.api.libs.json._
+import akka.pattern.pipe
 
 sealed trait PopMessage
 case class Start(eventId: Long) extends PopMessage
 case class Check(eventId: Long) extends PopMessage
 
 class PopulatorActor extends Actor {
-  var workingSet: Set[Long] = Set()
-
+  
   def receive = {
     case Start(eventId) => {
-      workingSet += eventId
+      addToSet(eventId)
 
       SingleEvent.get(eventId).map { e =>
         val fp = FlickrPopulator.populate(e)
@@ -29,19 +34,51 @@ class PopulatorActor extends Actor {
 
         Future.sequence(Seq(fp, ip, yp)).onComplete {
           case Success(Seq(f, i, y)) => {
-            workingSet -= eventId
+            removeFromSet(eventId)
             //Logger.debug(s"Found $f flickrs, $i instagrams, and $y youtubes")
           }
-          case Failure(e) => workingSet -= eventId; throw e
+          case Failure(e) => removeFromSet(eventId); throw e
         }
       }
     }
 
     case Check(eventId) => {
-      val stillWorking: Boolean = workingSet.contains(eventId)
+      val stillWorking = checkInSet(eventId)
 
-      sender ! stillWorking
+      stillWorking pipeTo sender
     }
 
+  }
+
+  def collection = ReactiveMongoPlugin.db.collection[JSONCollection]("currently_populating")
+
+  def addToSet(eventId: Long): Unit = {
+    val query = Json.obj()
+    val update = Json.obj(
+      "$addToSet" -> Json.obj(
+        "events" -> eventId
+      )
+    )
+
+    collection.update(query, update, upsert = true)
+  }
+
+  def removeFromSet(eventId: Long): Unit = {
+    val query = Json.obj()
+    val update = Json.obj(
+      "$pull" -> Json.obj(
+        "events" -> eventId
+      )
+    )
+
+    collection.update(query, update)
+  }
+
+  def checkInSet(eventId: Long): Future[Boolean] = {
+    val query = Json.obj(
+      "events" -> eventId
+    )
+
+    collection.find(query).one[JsValue].map(_.isDefined)
   }
 }
